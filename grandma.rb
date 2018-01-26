412dc15c2d444f29732bf2ff86e1bd874a68b921
Kim Foukaris <kimfoukaris@outlook.com>
2018-01-26 12:27:23 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -2,7 +2,7 @@ def speak_to_grandma(says)
   if says!=says.upcase
     return "HUH?! SPEAK UP, SONNY!"
   elsif "WHAT DID YOU EAT TODAY?"
-    return "NO, NOT SINCE 1938!"
+    puts "NO, NOT SINCE 1938!"
   else
     return "I LOVE YOU TOO PUMPKIN!"
   end
